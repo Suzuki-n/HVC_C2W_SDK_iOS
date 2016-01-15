448ebfed6a00528ab5c2c2bb22fedbeeeed00551
suzuki-n <vee00703@nifty.ne.jp>
2016-01-15 20:25:56 +0900

    Update main.m

--- sample/StandardDemo/StandardDemo/main.m
+++ sample/StandardDemo/StandardDemo/main.m
@@ -22,6 +22,7 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+// main
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
